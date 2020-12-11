c4a734f7c6dac0d7e4b2d93a08da9dde937b17d3
Kenneth Kopelson <kennethkopelson@gmail.com>
2020-12-11 15:06:56 -0800

    added comments

---
+++ routes/html-routes.js
@@ -0,0 +1,12 @@
+//HTML Routes
+
+module.exports = function(app) {
+
+  //Route for landing page "/"
+  //Route for user login "/login"
+  //Route for sign up "/signup"
+  //Route for bear list "/search"
+  //Route for user home page "/:user_name"
+  
+
+};
