69240fee1821034237f2f2cc5c3d16defe0eeed6
Joel Estrada <jre.estrada@gmail.com>
2020-12-12 13:10:22 -0800

    changed config.json to my credentials, added get route to api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,27 +2,37 @@
 module.exports = (app) => {
     //Passport routes
 
-        //Route for logging in a user "/api/login"
-        //Route for logging user out "/logout"
-        //Route to sign up a new user at "/api/signup"
+    //Route for logging in a user "/api/login"
+    //Route for logging user out "/logout"
+    //Route to sign up a new user at "/api/signup"
 
     //CREATE (POST)
 
-        //Route to create a new review on a user "/api/users/review"
+    //Route to create a new review on a user "/api/users/review"
 
-        //Route to create a new bear listing "/api/bears"
-        //Route to create a new review on a bear listing "/api/bears/review"
+    //Route to create a new bear listing "/api/bears"
+    //Route to create a new review on a bear listing "/api/bears/review"
 
 
     //READ (GET)
 
-        //Route to get all users information "/api/users"
+    //Route to get all users information "/api/users"
+
+    app.get("/api/bears", (req, res) => {
+        // Write code here to retrieve all of the bears from the database and res.json them
+        // back to the user
+        db.Bear.findAll({}).then(results => {
+            res.json(results);
+        }).catch(e => {
+            console.log(e);
+        });
+    });
 
 
     //Route to get all bears information "/api/bears"
-    app.get("/api/bears", function(req, res) {
+    app.get("/api/bears", function (req, res) {
         res.end();
-      });
+    });
     //Route to get all users with bear listings "/api/users/lists"
     //Route to get all bear with user listings "/api/bears/lists"
 
@@ -35,21 +45,21 @@ module.exports = (app) => {
     //Route to get a bear info by category "/api/bears/category/:category"
 
 
-//UPDATE (PUT)
+    //UPDATE (PUT)
 
     //Route to update a user from database "/api/users/id/:id"
-        //update username, password, listings, and reviews
+    //update username, password, listings, and reviews
     //Route to update a bear from database "/api/bears/id/:id"
-        //update name, value, picture, url, category
-        
+    //update name, value, picture, url, category
+
     //admin can update anything?
 
-//DELETE (DELETE) 
+    //DELETE (DELETE) 
 
     //Route to delete a user from database "/api/users/id/:id"
-        //user can delete their own review and listing
+    //user can delete their own review and listing
     //Route to delete a bear from database "/api/bears/id/:id"
-        //users can delete their own bears they added to the database?
-        
+    //users can delete their own bears they added to the database?
+
     //admin can delete anything?
 }
