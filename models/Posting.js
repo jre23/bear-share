1f24c906c9cb54ad23eb1be871397a6bd711ed68
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2020-12-12 13:34:55 -0800

    'Updated Tab Formatting'

--- models/Posting.js
+++ models/Posting.js
@@ -1,34 +1,34 @@
 module.exports = function (sequelize, DataTypes) {
-  var Posting = sequelize.define("Posting", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      },
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      len: [1],
-    },
-    image_paths: {
-      type: DataTypes.TEXT,
-    },
-    ask_price: {
-      type: DataTypes.INTEGER,
-    },
-  });
-
-  Posting.associate = function (models) {
-    // We're saying that a Posting should belong to an User
-    // A Posting can't be created without an User due to the foreign key constraint
-    Posting.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: false,
-      },
+    var Posting = sequelize.define("Posting", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1],
+        },
+        image_paths: {
+            type: DataTypes.TEXT,
+        },
+        ask_price: {
+            type: DataTypes.INTEGER,
+        },
     });
-  };
 
-  return Posting;
+    Posting.associate = function (models) {
+        // We're saying that a Posting should belong to an User
+        // A Posting can't be created without an User due to the foreign key constraint
+        Posting.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false,
+            },
+        });
+    };
+
+    return Posting;
 };
