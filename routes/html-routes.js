b6198add51b7f6fac0cfddd917cd9382ed35c2d6
Jehyun <congmul@hotmail.com>
2020-12-12 00:28:11 -0800

    Create Main & login & signup handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,10 @@
 module.exports = (app) => {
     app.get("/", (req, res) => {
+        console.log(req);
         res.render("index"); 
+    });
+
+    app.get("/signup", (req, res) => {
+        res.render("signup"); 
     });
 }
