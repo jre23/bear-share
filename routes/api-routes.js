9fa411864d92d9064f5ab0d6d3feb262011ef108
Kenneth <kennethkopelson@gmail.com>
2020-12-13 13:49:29 -0800

    added login and signup js (ajax) and fixed views id and classes and added login route and signup route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,44 @@
 //API Routes & passport routes
+
+// Requiring our models and passport as we've configured it
+const db = require("../models");
+const passport = require("../config/passport");
+
+
 module.exports = (app) => {
     //Passport routes
 
         //Route for logging in a user "/api/login"
-        //Route for logging user out "/logout"
-        //Route to sign up a new user at "/api/signup"
+        // Using the passport.authenticate middleware with our local strategy.
+        // If the user has valid login credentials, send them to the members page.
+        // Otherwise the user will be sent an error
+        app.post("/api/login", passport.authenticate("local"), function(req, res) {
+            res.json(req.user);
+        });
 
+
+
+        //Route for logging user out "/logout"
+        
+        //Route to sign up a new user at "/api/signup"
+        app.post("/api/signup", function(req, res) {
+            console.log(req);
+            db.User.create({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                phoneNumber: req.body.phoneNumber,
+                address: req.body.address,
+                email: req.body.email,
+                password: req.body.password
+            })
+              .then(function() {
+                res.redirect(307, "/api/login");
+              })
+              .catch(function(err) {
+                res.status(401).json(err);
+              });
+        });
+        
     //CREATE (POST)
 
         //Route to create a new review on a user "/api/users/review"
