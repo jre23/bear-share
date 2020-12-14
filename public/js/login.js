a7a431335978e86d2fe10dcf852208f51bf30065
Ken Kopelson <kennethkopelson@gmail.com>
2020-12-13 17:33:57 -0800

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-    // Getting references to our form and inputs
-    const loginForm = $(".login");
-    const emailInput = $("#email");
-    const passwordInput = $("#password");
-  
-    // When the form is submitted, we validate there's an email and password entered
-    loginForm.on("submit", function(event) {
-      event.preventDefault();
-      const userData = {
-        email: emailInput.val().trim(),
-        password: passwordInput.val().trim()
-      };
-  
-      if (!userData.email || !userData.password) {
-        return;
-      }
-  
-      // If we have an email and password we run the loginUser function and clear the form
-      loginUser(userData.email, userData.password);
-      emailInput.val("");
-      passwordInput.val("");
-    });
-  
-    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-    function loginUser(email, password) {
-      $.post("/api/login", {
-        email: email,
-        password: password
-      })
-        .then(function() {
-          window.location.replace("/");
-          // If there's an error, log the error
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
-    }
-  });
-  
-=======
 $(document).ready(() => {
     const loginForm = $("form.login");
     const emailLogin = $("input#email_login");
@@ -72,7 +30,7 @@ $(document).ready(() => {
             password: password
         })
             .then(function () {
-                window.location.replace("/members");
+                window.location.replace("/");
                 // If there's an error, log the error
             })
             .catch(function (err) {
@@ -81,4 +39,3 @@ $(document).ready(() => {
     }
 
 });
->>>>>>> 3b10e46d8f39e0966757ffc03366e7df7e05c5a8
